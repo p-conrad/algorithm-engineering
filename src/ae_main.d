67204a516a12eb2ea4a9ea55a05bfbb4f3a5d241
Peter Conrad <yako.conrad.peter@gmail.com>
2015-01-09 19:35:19 +0100

    Make measurements callable from main function
    Amend the program file to request input from the user, based on which
    a wall time measurement or a cycle count measurement of the fibonacci
    algorithms is performed, or the program is quit without doing anything.

--- src/ae_main.d
+++ src/ae_main.d
@@ -1,5 +1,42 @@
+import std.stdio;
 import fibonacci;
+import meter;
+import measurement;
+
+void timeMeasurements() {
+		auto measurement = new FibMeasurement(new WallMeter());
+		measurement.doMeasurement();
+}
+
+void countCycles() {
+		auto measurement = new FibMeasurement(new CycleMeter());
+		measurement.doMeasurement();
+}
+
+int getUserInput() {
+	int option;
+	writeln("***** MAIN *****");
+	writeln("(1) Do a wall time measurement of the fibonacci algorithms");
+	writeln("(2) Do a cycle count measurement of the fibonacci algorithms");
+	writeln("(3) Quit and do nothing");
+	write("Selection: ");
+	readf("%s", &option);
+
+	return option;
+}
 
 void main() {
-	// nothing to see yet
+	int option = getUserInput();
+
+	switch (option) {
+		case 1:
+			timeMeasurements();
+			break;
+		case 2:
+			countCycles();
+			break;
+		case 3:
+		default:
+			{ }
+	}
 }
