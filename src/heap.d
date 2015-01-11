c1d7129b9cddc3c0043fe1ee8c9b53d3c00485b0
Peter Conrad <yako.conrad.peter@gmail.com>
2015-01-11 16:01:19 +0100

    Add invariants to maxHeapify
    Implement another function checking whether a given vector is a heap at
    a given point. Use that function to check the pre- and post-conditions
    of the maxHeapify algorithm.

--- src/heap.d
+++ src/heap.d
@@ -28,12 +28,31 @@ bool isHeap(T)(T[] array) {
 	return true;
 }
 
+bool isHeap(T)(T[] array, size_t i) {
+	if ((left(i) < array.length && array[i] < array[left(i)])
+			|| (right(i) < array.length && array[i] < array[right(i)]))
+		return false;
+	else if (left(i) < array.length && right(i) <= array.length)
+		return isHeap(array, left(i)) && isHeap(array, right(i));
+	else if (left(i) < array.length)
+		return isHeap(array, left(i));
+	else if (right(i) < array.length)
+		return isHeap(array, right(i));
+	else return true;
+}
+
 void maxHeapify(T)(T[] array, size_t i) {
 	import std.algorithm;
 
 	auto l = left(i);
 	auto r = right(i);
 	auto max = i;
+
+	// pre-condition: the left and right child of i are heaps
+	if (l < array.length)
+		assert(isHeap(array, l));
+	if (r < array.length)
+		assert(isHeap(array, r));
 
 	if ((l < array.length) && (array[l] > array[i]))
 		max = l;
@@ -45,6 +64,9 @@ void maxHeapify(T)(T[] array, size_t i) {
 		swap(array[i], array[max]);
 		maxHeapify(array, max);
 	}
+
+	// post-condition: i is a heap
+	assert (isHeap(array, i));
 }
 
 void buildMaxHeap(T)(T[] array) {
