431f8287c149230a17f39ff3e3d282165122f966
Peter Conrad <yako.conrad.peter@gmail.com>
2015-01-31 15:34:37 +0100

    Change graph representation to int[vert][vert]
    Discard AdjElement in favor to representing a graph using an associative
    array of associative arrays, whose elements would equal the weight of an
    edge. A representation of an adjacency list using AdjElement gains a
    little bit of code readability, however it brings the disadvantage of
    having to handle duplicates upon isertion or other operations on the
    graph, as well as some complexity deleting them from the array (which
    would need to be rearranged). Using an associative array duplicates can,
    by definition, not occur. Furthermore getting the weight of an edge
    becomes as simple as just accessing it by its index, and deletion can be
    realized using the remove() function. Change the functions accordingly.

--- src/graph.d
+++ src/graph.d
@@ -3,49 +3,46 @@
 // switch to another identifier.
 alias vert = int;
 
-// List element of an adjacency list containing the connecting node
-// and the weight of the connecting edge. 
-import std.typecons;
-alias AdjElement = Tuple!(vert, "vertex", int, "weight");
-
 // A graph will be represented by an adjacency list.
 // An adjacency list is stored as an associative array, where the key
-// equals the identifier of a vertex in a graph.
+// equals the identifier of a vertex in a graph, containing another
+// associatve array of adjacent nodes. The value then equals the weight
+// of the edge.
 // A simple array would not be sufficient, because it would, for instance,
 // not allow us to generate subsets of a given graph.
-alias Graph = AdjElement[][vert];
+alias Graph = int[vert][vert];
 
 // check whether two given vertices are connected
 bool adjacent(in Graph g, in vert a, in vert b) {
 	if (!((a in g) && (b in g))) return false;
 
-	import std.algorithm : find;
-	return find!("a.vertex == b")(g[a], b).length > 0;
+	return cast(bool)(b in g[a]);
 }
 
 // insert a single vertex into a graph, not having any adjacent edges
 bool insertVertex(ref Graph g, vert n) {
 	if (n in g) return false;
 	else {
-		g[n] = [];
+		g[n] = g[n].init;
 		return true;
 	}
 }
 
 // insert a new edge into a graph
-bool insertEdge(ref Graph g, vert source, vert target, int weight = 1,
+bool insertEdge(ref Graph g, vert a, vert b, int weight = 1,
 		bool insertNew = false) {
-	if ((!(source in g) || !(target in g)) && !insertNew)
+	if ((!(a in g) || !(b in g)) && !insertNew)
 		return false;
-	if (adjacent(g, source, target)) return false;
+	if (adjacent(g, a, b)) return false;
 	
 	// Just insert the vertices. Nothing will happen here if they
 	// already exist
-	insertVertex(g, source);
-	insertVertex(g, target);
+	insertVertex(g, a);
+	insertVertex(g, b);
 
-	g[source] ~= AdjElement(target, weight);
-	g[target] ~= AdjElement(source, weight);
+	g[a][b] = weight;
+	g[b][a] = weight;
+
 	return true;
 }
 
@@ -57,5 +54,6 @@ unittest {
 	insertVertex(g, 5);
 	assert (insertEdge(g, 1, 5, 3));
 	assert (adjacent(g, 1, 5));
+	assert (adjacent(g, 5, 1));
 	assert (!insertEdge(g, 1, 5));
 }
