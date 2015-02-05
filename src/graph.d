afbaf1171f1e86b05d30fe0bce46f65bc337fde7
Peter Conrad <yako.conrad.peter@gmail.com>
2015-02-05 11:52:27 +0100

    Generalize graph definitions
    Make the definition of a graph generic, having a template parameter for
    each the type of the vertex and the type of the edge. This way, graphs
    consisting of any data type can be constructed. Apart from the simple
    case of generating graphs holding other data types, for instance, a
    depth-first search algorithm could too be implemented in a way that the
    types of found edges are stored along with their weight. If no
    parameters are specified, a default type will be used. Modify all
    functions and unit tests to comply with the new definition.

--- src/graph.d
+++ src/graph.d
@@ -1,9 +1,8 @@
 module graph;
 
-// Vertices are identified given an int value. Define symbolic alias
-// for them to gain some readability. This will also enable us to easily
-// switch to another identifier.
-alias vert = int;
+// default data types for vertices and edges
+alias vType = int;
+alias eType = int;
 
 // A graph will be represented by an adjacency list.
 // An adjacency list is stored as an associative array, where the key
@@ -12,23 +11,24 @@ alias vert = int;
 // of the edge.
 // A simple array would not be sufficient here, because it would, for
 // instance, not allow us to generate subsets of a given graph.
-alias Graph = int[vert][vert];
+alias Graph(V = vType, E = eType) = E[V][V];
 
 // A tuple representing an edge of the graph. This enables us to store
 // edges of the graph outside an adjacency list, e.g. in a prioriy
-// queue.
+// queue. The tuple is defined in a way that allows us to expand it into
+// other functions, e.g. insertEdge()
 import std.typecons;
-alias Edge = Tuple!(vert, "a", vert, "b", int, "weight");
+alias Edge(V = vType, E = eType) = Tuple!(V, "a", V, "b", E, "weight");
 
 // check whether two given vertices are connected
-bool adjacent(in Graph g, in vert a, in vert b) {
+bool adjacent(V = vType, E = eType)(in Graph!(V, E) g, in V a, in V b) {
 	if (!((a in g) && (b in g))) return false;
 
 	return cast(bool)(b in g[a]);
 }
 
 // insert a single vertex into a graph, not having any adjacent edges
-bool insertVertex(ref Graph g, vert n) {
+bool insertVertex(V = vType, E = eType)(ref Graph!(V, E) g, V n) {
 	if (n in g) return false;
 	else {
 		g[n] = g[n].init;
@@ -37,16 +37,16 @@ bool insertVertex(ref Graph g, vert n) {
 }
 
 // insert a new edge into a graph. Existing edges will be overridden.
-bool insertEdge(ref Graph g, vert a, vert b, int weight = 1,
-		bool oneWay = false, bool insertNew = true) {
+bool insertEdge(V = vType, E = eType)(ref Graph!(V, E) g, V a, V b,
+		E weight = E.init, bool oneWay = false, bool insertNew = true) {
 	if ((!(a in g) || !(b in g)) && !insertNew)
 		return false;
-	if (adjacent(g, a, b)) return false;
+	if (adjacent!(V, E)(g, a, b)) return false;
 	
 	// Just insert the vertices. Nothing will happen here if they
 	// already exist
-	insertVertex(g, a);
-	insertVertex(g, b);
+	insertVertex!(V, E)(g, a);
+	insertVertex!(V, E)(g, b);
 
 	g[a][b] = weight;
 	if (!oneWay)
@@ -56,7 +56,8 @@ bool insertEdge(ref Graph g, vert a, vert b, int weight = 1,
 }
 
 // remove an edge from the graph
-bool removeEdge(ref Graph g, vert a, vert b, bool oneWay = false) {
+bool removeEdge(V = vType, E = eType)(ref Graph!(V, E) g, V a, V b,
+		bool oneWay = false) {
 	if ((a in g) && (b in g)) {
 		g[a].remove(b);
 		if (!oneWay)
@@ -67,8 +68,8 @@ bool removeEdge(ref Graph g, vert a, vert b, bool oneWay = false) {
 }
 
 // deep-copy a graph and all of its elements
-Graph clone(Graph g) {
-	Graph clone;
+Graph!(V, E) clone(V = vType, E = eType)(Graph!(V, E) g) {
+	Graph!(V, E) clone;
 	foreach (e; g.byKey())
 		clone[e] = g[e].dup;
 	return clone;
@@ -76,15 +77,15 @@ Graph clone(Graph g) {
 
 // generate an array of the edges from a given graph, ignoring
 // any duplicates
-Edge[] toArray(in Graph g) {
-	Graph finished;
-	Edge[] result;
+Edge!(V, E)[] toArray(V = vType, E = eType)(in Graph!(V, E) g) {
+	Graph!(V, E) finished;
+	Edge!(V, E)[] result;
 
 	foreach (a; g.byKey()) {
 		foreach (b; g[a].byKey()) {
-			if (!adjacent(finished, a, b)) {
-				result ~= Edge(a, b, g[a][b]);
-				insertEdge(finished, a, b);
+			if (!adjacent!(V, E)(finished, a, b)) {
+				result ~= Edge!(V, E)(a, b, g[a][b]);
+				insertEdge!(V, E)(finished, a, b);
 			}
 		}
 	}
@@ -92,13 +93,13 @@ Edge[] toArray(in Graph g) {
 }
 
 // construct a graph from a given set of vertices and edges
-Graph construct(vert[] v, Edge[] e = []) {
-	Graph result;
+Graph!(V, E) construct(V = vType, E = eType)(V[] v, Edge!(V, E)[] e = []) {
+	Graph!(V, E) result;
 	
 	foreach (a; v)
-		insertVertex(result, a);
+		insertVertex!(V, E)(result, a);
 	foreach (a; e)
-		insertEdge(result, a.expand);
+		insertEdge!(V, E)(result, a.expand);
 
 	return result;
 }
@@ -107,23 +108,23 @@ Graph construct(vert[] v, Edge[] e = []) {
 // in its adjacency list if and only if (b, a) is also contained.
 // Although correct, this information is redundant, so generate a graph
 // where each edge is only represented once.
-Graph reduce(in Graph g) {
-	Graph result;
+Graph!(V, E) reduce(V = vType, E = eType)(in Graph!(V, E) g) {
+	Graph!(V, E) result;
 
-	auto array = toArray(g);
+	auto array = toArray!(V, E)(g);
 	foreach(a; array)
-		insertEdge(result, a.expand, true);
+		insertEdge!(V, E)(result, a.expand, true);
 
 	return result;
 }
 
 // restore a previously reduced graph to its normal representation
-Graph restore(in Graph g) {
-	Graph result;
+Graph!(V, E) restore(V = vType, E = eType)(in Graph!(V, E) g) {
+	Graph!(V, E) result;
 
 	foreach (a; g.byKey()) {
 		foreach (b; g[a].byKey())
-			insertEdge(result, a, b, g[a][b]);
+			insertEdge!(V, E)(result, a, b, g[a][b]);
 	}
 
 	return result;
@@ -134,17 +135,18 @@ Graph restore(in Graph g) {
 // determining spanning trees. Safe edges are any edges of which one
 // adjacent node is part of the existing subset, and the other one
 // is not.
-Edge safeEdge(in Graph g, in Graph sub) {
+Edge!(V, E) safeEdge(V = vType, E = eType)(in Graph!(V, E) g,
+		in Graph!(V, E) sub) {
 	// sub must be an actual subset of g
 	foreach (v; sub.byKey())
 		assert (v in g);
 
-	Edge[] edges;
+	Edge!(V, E)[] edges;
 
 	foreach (a; sub.byKey()) {
 		foreach (b; g[a].byKey()) {
 			if (!(b in sub))
-				edges ~= Edge(a, b, g[a][b]);
+				edges ~= Edge!(V, E)(a, b, g[a][b]);
 		}
 	}
 	
@@ -156,74 +158,74 @@ Edge safeEdge(in Graph g, in Graph sub) {
 		auto min = minPos!("a.weight < b.weight")(edges)[0];
 		return min;
 	}
-	else return Edge.init;
+	else return Edge!(V, E).init;
 }
 
 // a simple algorithm used to generate a minimum spanning tree by
 // repeatedly inserting safe edges. This will only work if the
 // components are connected
-Graph genericMST(Graph g) {
+Graph!(V, E) genericMST(V = vType, E = eType)(Graph!(V, E) g) {
 	if (g.length == 0) return g;
-	Graph tree = construct([g.keys[0]]);
+	auto tree = construct!(V, E)([g.keys[0]]);
 	
-	auto edge = safeEdge(g, tree);
-	while (edge != Edge.init) {
-		insertEdge(tree, edge.expand);
-		edge = safeEdge(g, tree);
+	auto edge = safeEdge!(V, E)(g, tree);
+	while (edge != Edge!(V, E).init) {
+		insertEdge!(V, E)(tree, edge.expand);
+		edge = safeEdge!(V, E)(g, tree);
 	}
 
 	return tree;
 }
 
 unittest {
-	Graph g;
+	Graph!() g;
 
 	// inserting vertices
-	assert(insertVertex(g, 1));
-	assert(!insertVertex(g, 1));
+	assert(insertVertex!()(g, 1));
+	assert(!insertVertex!()(g, 1));
 	assert(1 in g);
-	insertVertex(g, 2);
-	insertVertex(g, 3);
+	insertVertex!()(g, 2);
+	insertVertex!()(g, 3);
 
 	// inserting edges, checking weights etc.
-	assert(insertEdge(g, 1, 2, 5));
-	assert(!insertEdge(g, 1, 2));
-	assert(adjacent(g, 1, 2));
-	assert(adjacent(g, 2, 1));
+	assert(insertEdge!()(g, 1, 2, 5));
+	assert(!insertEdge!()(g, 1, 2));
+	assert(adjacent!()(g, 1, 2));
+	assert(adjacent!()(g, 2, 1));
 	assert(g[1][2] == 5);
 	assert(g[2][1] == 5);
-	insertEdge(g, 2, 3, 3);
-	insertEdge(g, 3, 1, 6);
+	insertEdge!()(g, 2, 3, 3);
+	insertEdge!()(g, 3, 1, 6);
 
 	// inserting and removing edges one-directional
-	assert(removeEdge(g, 1, 2, true));
-	assert(!adjacent(g, 1, 2));
-	assert(adjacent(g, 2, 1));
-	assert(insertEdge(g, 1, 2, 10, true));
+	assert(removeEdge!()(g, 1, 2, true));
+	assert(!adjacent!()(g, 1, 2));
+	assert(adjacent!()(g, 2, 1));
+	assert(insertEdge!()(g, 1, 2, 10, true));
 	assert(g[1][2] == 10);
 	assert(g[2][1] == 5);
 
 	// reducing the graph
-	g = reduce(g);
-	assert(adjacent(g, 1, 2));
-	assert(!adjacent(g, 2, 1));
+	g = reduce!()(g);
+	assert(adjacent!()(g, 1, 2));
+	assert(!adjacent!()(g, 2, 1));
 
 	// restoring the reduced graph
-	g = restore(g);
-	assert(adjacent(g, 1, 2));
-	assert(adjacent(g, 2, 1));
+	g = restore!()(g);
+	assert(adjacent!()(g, 1, 2));
+	assert(adjacent!()(g, 2, 1));
 
 	// construct a graph using the given function
-	g = construct([1, 2, 3], [Edge(1, 2, 3), Edge(2, 3, 6), Edge(3, 1, 9)]);
+	g = construct!()([1, 2, 3], [Edge!()(1, 2, 3), Edge!()(2, 3, 6), Edge!()(3, 1, 9)]);
 	assert(1 in g);
-	assert(adjacent(g, 1, 2));
-	assert(adjacent(g, 2, 1));
+	assert(adjacent!()(g, 1, 2));
+	assert(adjacent!()(g, 2, 1));
 	
 	// finding safe edges for g, starting from 1
-	auto tree = construct([1]);
-	assert(safeEdge(g, tree) == Edge(1, 2, 3));
-	insertEdge(tree, safeEdge(g, tree).expand);
-	assert(safeEdge(g, tree) == Edge(2, 3, 6));
-	insertEdge(tree, safeEdge(g, tree).expand);
-	assert(safeEdge(g, tree) == Edge.init);
+	auto tree = construct!()([1]);
+	assert(safeEdge!()(g, tree) == Edge!()(1, 2, 3));
+	insertEdge!()(tree, safeEdge!()(g, tree).expand);
+	assert(safeEdge!()(g, tree) == Edge!()(2, 3, 6));
+	insertEdge!()(tree, safeEdge!()(g, tree).expand);
+	assert(safeEdge!()(g, tree) == Edge!().init);
 }
